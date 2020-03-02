1f96cf5005aaf0cbf9b78700e40061df46f34011
Carolina Soares <m.carolina.soares1@gmail.com>
2020-03-02 12:24:21 -0300

    add variaveis de configurações

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['m.carolina.soares1@gmail.com']
     LANGUAGES = ['en']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     POSTS_PER_PAGE = 25
