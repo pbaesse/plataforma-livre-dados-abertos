82cabdc057d60908a3e89785d2aeb450326ec77b
Carolina Soares <m.carolina.soares1@gmail.com>
2020-03-01 23:11:58 -0300

    criei arquivo para testes

---
+++ test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from datetime import datetime, timedelta
+import unittest
+from app import create_app, db
+from app.models import User, Post
+from config import Config
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_password_hashing(self):
+        u = User(username='susan')
+        u.set_password('cat')
+        self.assertFalse(u.check_password('dog'))
+        self.assertTrue(u.check_password('cat'))
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
