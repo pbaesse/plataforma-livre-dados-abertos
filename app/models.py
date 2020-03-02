f0c9fb3e48de9728082673c37fe3678bd8f91eb1
Carolina Soares <m.carolina.soares1@gmail.com>
2020-03-01 23:02:37 -0300

    criei estrutura blueprint

--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
 from datetime import datetime
+from hashlib import md5
 from time import time
+from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import app, db, login
+import jwt
+from app import db, login
 
 class User(UserMixin, db.Model):
 	__tablename__ = "users"
-	
+
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(200), index=True, unique=True)
 	email = db.Column(db.String(200), index=True, unique=True)
@@ -15,13 +18,13 @@ class User(UserMixin, db.Model):
 	nickname = db.Column(db.String(150))
 	typeUser = db.Column(db.String(200))
 	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-	
+
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
-		
+
 	def set_password(self, password):
 		self.password_hash = generate_password_hash(password)
-		
+
 	def check_password(self, password):
 		return check_password_hash(self.password_hash, password)
 
@@ -31,7 +34,7 @@ def load_user(id):
 
 class Source(db.Model):
 	__tablename__ = "sources"
-	
+
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(200), index=True, unique=True)
 	sphere = db.Column(db.String(200), index=True)
@@ -40,15 +43,15 @@ class Source(db.Model):
 	datasetLink = db.Column(db.String(300), index=True)
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-	
+
 	user = db.relationship('User', backref=db.backref('sources', lazy=True))
-	
+
 	def __repr__(self):
 		return '<Source {}>'.format(self.title)
-        
+
 class Software(db.Model):
 	__tablename__ = "softwares"
-	
+
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(200), index=True, unique=True)
 	description = db.Column(db.String(800), index=True)
@@ -59,29 +62,29 @@ class Software(db.Model):
 	dateCreation = db.Column(db.String(300), index=True)
 	dateRelease = db.Column(db.String(300), index=True)
 	user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-	
+
 	user = db.relationship('User', backref=db.backref('softwares', lazy=True))
-	
+
 	def __repr__(self):
 		return '<Software {}>'.format(self.title)
-               
+
 class Tag(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	palavraChave = db.Column(db.String(200), index=True)
-	
+
 	def __repr__(self):
 		return '<Tag{}>'.format(self.palavraChave)
 
 class Categoria(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	
+
 	def __repr__(self):
-		return '<Categoria {}>'.format(self.id) 
+		return '<Categoria {}>'.format(self.id)
 
 class Favorito(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	gostei = db.Column(db.String(200), index=True)
-	
+
 	def __repr__(self):
 		return '<Favorito {}>'.format(self.gostei)
 
@@ -89,14 +92,14 @@ class Comentario(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	data = db.Column(db.String(200), index=True)
 	texto = db.Column(db.String(500), index=True)
-	
+
 	def __repr__(self):
 		return '<Comentario {}>'.format(self.data)
-        
+
 class Denuncia(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	descricao = db.Column(db.String(500), index=True)
 	tipo = db.Column(db.String(200), index=True)
-	
+
 	def __repr__(self):
 		return '<Denuncia {}>'.format(self.tipo)
