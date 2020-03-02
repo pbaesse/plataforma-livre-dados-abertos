9c23f39f3df99e8bead0d9e8de68928a27645e81
Carolina Soares <m.carolina.soares1@gmail.com>
2020-03-01 23:08:41 -0300

    criei diretório main

---
+++ app/main/form.py
@@ -0,0 +1,6 @@
+from flask import request
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Length
+from flask_babel import _, lazy_gettext as _l
+from app.models import User
---
+++ app/main/routes.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from flask import render_template, flash, redirect, url_for, request, g, \
+    jsonify, current_app
+from flask_login import current_user, login_required
+from flask_babel import _, get_locale
+from guess_language import guess_language
+from app import db
+from app.main.forms import EditProfileForm, PostForm
+from app.models import User, Post
+from app.translate import translate
+from app.main import bp
