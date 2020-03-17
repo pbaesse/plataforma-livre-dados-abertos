e7511e72e084ad95f66fd6bf82172434999df197
Carolina Soares <m.carolina.soares1@gmail.com>
2020-03-17 10:45:41 -0300

    add models de seguido e seguidor

--- app/models.py
+++ app/models.py
@@ -8,6 +8,12 @@ import jwt
 from app import db, login
 
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('users.id'))
+)
+
+
 class User(UserMixin, db.Model):
 	__tablename__ = "users"
 
@@ -19,9 +25,38 @@ class User(UserMixin, db.Model):
 	nickname = db.Column(db.String(150))
 	typeUser = db.Column(db.String(200))
 	last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+	followed = db.relationship(
+		'User', secondary=followers,
+		primaryjoin=(followers.c.follower_id == id),
+		secondaryjoin=(followers.c.followed_id == id),
+		backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
 	def __repr__(self):
 		return '<User {}>'.format(self.username)
+
+	def avatar(self, size):
+		digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+		return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+	def follow(self, user):
+		if not self.is_following(user):
+			self.followed.append(user)
+
+	def unfollow(self, user):
+		if self.is_following(user):
+			self.followed.remove(user)
+
+	def is_following(self, user):
+		return self.followed.filter(
+			followers.c.followed_id == user.id).count() > 0
+
+	def followed_posts(self):
+		followed = Post.query.join(
+			followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+		own = Post.query.filter_by(user_id=self.id)
+		return followed.union(own).order_by(Post.timestamp.desc())
 
 	def set_password(self, password):
 		self.password_hash = generate_password_hash(password)
