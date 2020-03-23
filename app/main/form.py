0543b08e21ff9a43f42d7a69d2039c4840204e6e
Carolina Soares <m.carolina.soares1@gmail.com>
2020-03-23 01:59:34 -0300

    atualizei formulários

--- app/main/form.py
+++ app/main/form.py
@@ -1,9 +1,21 @@
 from flask import request
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Length, Email, EqualTo
+from flask_wtf.html5 import URLField
+from wtforms import StringField, SelectField, TextAreaField, DateField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Length, Email, EqualTo, url
 from flask_babel import _, lazy_gettext as _l
 from app.models import User, Post, Software
+
+
+class SearchForm(FlaskForm):
+    q = StringField(_l('Search'), validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        if 'formdata' not in kwargs:
+            kwargs['formdata'] = request.args
+        if 'csrf_enabled' not in kwargs:
+            kwargs['csrf_enabled'] = False
+        super(SearchForm, self).__init__(*args, **kwargs)
 
 
 class EditProfileForm(FlaskForm):
@@ -24,21 +36,23 @@ class EditProfileForm(FlaskForm):
 
 
 class PostForm(FlaskForm):
-	title = StringField(_l('Título'), validators=[DataRequired()])
-	tag = StringField(_l('Tag'), validators=[DataRequired()])
-	sphere = StringField(_l('Esfera'), validators=[DataRequired()])
-	officialLink = StringField(_l('Link Oficial'), validators=[DataRequired()])
-	description = TextAreaField(_l('Descrição'), validators=[DataRequired()])
+	title = StringField(_l('Título:'), validators=[DataRequired()])
+	tag = StringField(_l('Palavra-Chave:'), validators=[DataRequired()])
+	sphere = SelectField('Esfera:', choices=[('Municipal', 'Municipal'),
+		('Estadual', 'Estadual'), ('Federal', 'Federal'),
+		('Internacional','Internacional')], validators=[DataRequired()])
+	description = TextAreaField(_l('Descrição:'), validators=[DataRequired()])
+	officialLink = URLField(_l('Link Oficial:'), validators=[DataRequired(), url()])
 	submit = SubmitField(_l('Enviar'))
 
 
 class SoftwareForm(FlaskForm):
 	title = StringField(_l('Título'), validators=[DataRequired()])
-	tag = StringField(_l('Tag'), validators=[DataRequired()])
-	license = StringField(_l('Licença'), validators=[DataRequired()])
-	owner = StringField(_l('Proprietário'), validators=[DataRequired()])
-	activeDevelopment = StringField(_l('Desenvolvedor Ativo'), validators=[DataRequired()])
-	downloadLink = StringField(_l('Link para Download'), validators=[DataRequired()])
-	dateCreation = StringField(_l('Data de Criação'), validators=[DataRequired()])
+	tag = StringField(_l('Palavra-Chave:'), validators=[DataRequired()])
+	license = StringField(_l('Licença:'), validators=[DataRequired()])
+	owner = StringField(_l('Proprietário:'), validators=[DataRequired()])
+	activeDevelopment = StringField(_l('Desenvolvedor Ativo:'), validators=[DataRequired()])
+	downloadLink = URLField(_l('Link para Download:'), validators=[DataRequired()])
+	dateCreation = DateField(_l('Data de Criação:', format='%m/%d%y'), validators=[DataRequired()])
 	description = TextAreaField(_l('Descrição'), validators=[DataRequired()])
 	submit = SubmitField(_l('Enviar'))
