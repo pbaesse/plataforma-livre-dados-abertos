76a782a87d990dcb058f0518a1edd74c08b538a4
Carolina Soares <m.carolina.soares1@gmail.com>
2020-03-23 01:59:44 -0300

    atualizei rotas

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,16 +5,31 @@ from flask_login import current_user, login_required
 from flask_babel import _, get_locale
 from guess_language import guess_language
 from app import db
-from app.main.form import EditProfileForm, PostForm, SoftwareForm
+from app.main.form import EditProfileForm, PostForm, SoftwareForm, SearchForm
 from app.models import User, Post, Software
 #from app.translate import translate
 from app.main import bp
+
+@bp.route('/search')
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for('main.index'))
+    page = request.args.get('page', 1, type=int)
+    posts, total = Post.search(g.search_form.q.data, page,
+                               current_app.config['POSTS_PER_PAGE'])
+    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
+        if total > page * current_app.config['POSTS_PER_PAGE'] else None
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
+        if page > 1 else None
+    return render_template('search.html', title=_('Search'), posts=posts,
+                           next_url=next_url, prev_url=prev_url)
 
 @bp.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+        g.search_form = SearchForm()
     g.locale = str(get_locale())
 
 @bp.route('/', methods=['GET', 'POST'])
