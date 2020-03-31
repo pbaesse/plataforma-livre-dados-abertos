df07cb1e248f8f0376565ed698757f4cf2bd533e
Carolina Soares <m.carolina.soares1@gmail.com>
2020-03-30 21:02:20 -0300

    atualizei form post url

--- app/main/form.py
+++ app/main/form.py
@@ -1,8 +1,7 @@
 from flask import request
 from flask_wtf import FlaskForm
-from flask_wtf.html5 import URLField
 from wtforms import StringField, SelectField, TextAreaField, DateField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Length, Email, EqualTo, url
+from wtforms.validators import ValidationError, DataRequired, Length, Email, EqualTo, Regexp
 from flask_babel import _, lazy_gettext as _l
 from app.models import User, Post, Software
 
@@ -43,7 +42,9 @@ class PostForm(FlaskForm):
         ('Internacional','Internacional')], validators=[DataRequired()])
     categorie = StringField(_l('Categoria'), validators=[DataRequired()])
     description = TextAreaField(_l('Descrição:'), validators=[DataRequired()])
-    officialLink = URLField(_l('Link Oficial:'), validators=[DataRequired(), url()])
+    officialLink = StringField(_l('Link Oficial:'), validators=[DataRequired('URL verificada!'),
+        Regexp('^(http|https):\/\/[\w.\-]+(\.[\w.\-]+)+.*$', 0,
+               'URL inválida')])
     submit = SubmitField(_l('Enviar'))
 
 
@@ -54,7 +55,7 @@ class SoftwareForm(FlaskForm):
     license = StringField(_l('Licença:'), validators=[DataRequired()])
     owner = StringField(_l('Proprietário:'), validators=[DataRequired()])
     activeDevelopment = StringField(_l('Desenvolvedor Ativo:'), validators=[DataRequired()])
-    downloadLink = URLField(_l('Link para Download:'), validators=[DataRequired()])
+    downloadLink = StringField(_l('Link para Download:'), validators=[DataRequired()])
     dateCreation = StringField(_l('Data de Criação:'), validators=[DataRequired()])
     description = TextAreaField(_l('Descrição'), validators=[DataRequired()])
     submit = SubmitField(_l('Enviar'))
