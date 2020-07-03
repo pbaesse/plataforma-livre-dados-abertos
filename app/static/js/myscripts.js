14297bbcd9827712fc3cbf3021f6074ac58299a9
mariacarolinass <m.carolina.soares1@gmail.com>
2020-07-02 22:51:12 -0300

    fiz o semelhantes para fontes

--- app/static/js/myscripts.js
+++ app/static/js/myscripts.js
@@ -1,17 +1,19 @@
-$(document).ready(function(){
-  var titles=[];
+$(document).ready(function () {
+  var titles = [
 
-  function loadTitles(){
-      $.getJSON('/_autocomplete', function(data, status, xhr){
-        for (var i = 0; i < data.length; i++ ) {
-          titles.push(data[i].title);
-        }
-      });
-    };
+  ];
 
-    loadTitles();
-
-    $('#autocomplete').autocomplete({
-      source: titles
+  function loadTitles() {
+    $.getJSON('/_autocomplete', function (data, status, xhr) {
+      for (var i = 0; i < data.length; i++) {
+        titles.push(data[i].title);
+      }
     });
+  };
+
+  loadTitles();
+  
+  $('#autocomplete').autocomplete({
+    source: titles
   });
+});
