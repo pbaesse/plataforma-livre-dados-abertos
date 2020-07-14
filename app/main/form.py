a6d0c6bc4a96af725ddb7bcb56d6a0ea28d05ebc
mariacarolinass <m.carolina.soares1@gmail.com>
2020-07-14 00:17:22 -0300

    add placeholder aos formss

--- app/main/form.py
+++ app/main/form.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python -*- coding: utf-8 -*-
 from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextField, TextAreaField, DateField, PasswordField, BooleanField, SubmitField
@@ -20,10 +19,13 @@ class SearchForm(FlaskForm):
 
 
 class EditProfileForm(FlaskForm):
-	username = StringField(_l('Nome'), validators=[DataRequired()])
-	nickname = StringField(_l('Apelido'), validators=[DataRequired()])
-	about_me = TextAreaField(_l('Sobre mim'), validators=[Length(min=0, max=200)])
-	submit = SubmitField(_l('Enviar'))
+	username = StringField(_l('Nome'), validators=[DataRequired(),
+        Length(min=3)], render_kw={"placeholder": "Digite um nome de usuário"})
+	nickname = StringField(_l('Apelido'), validators=[DataRequired(),
+        Length(min=2)], render_kw={"placeholder": "Digite seu apelido de usuário"})
+	about_me = TextAreaField(_l('Sobre mim'), validators=[Length(max=150)],
+        render_kw={"placeholder": "Digite uma breve descrição sobre você"})
+	submit = SubmitField(_l('Salvar'))
 
 	def __init__(self, original_username, *args, **kwargs):
 		super(EditProfileForm, self).__init__(*args, **kwargs)
@@ -33,32 +35,35 @@ class EditProfileForm(FlaskForm):
 		if username.data != self.original_username:
 			user = User.query.filter_by(username=self.username.data).first()
 			if user is not None:
-				raise ValidationError(_('Por favor digite um nome diferente.'))
+				raise ValidationError(_('Escolha um nome diferente'))
 
 
 class PostForm(FlaskForm):
-    title = StringField(_l('Título:'), validators=[DataRequired()])
-    tag = StringField(_l('Palavra-Chave:'), validators=[DataRequired()])
-    sphere = SelectField('Esfera:', choices=[('Municipal', 'Municipal'),
+    title = StringField(_l('Título'), validators=[DataRequired(),
+        Length(min=3)], render_kw={"placeholder": "Digite o título da fonte de dados"})
+    tag = StringField(_l('Palavra-Chave'), validators=[DataRequired()])
+    sphere = SelectField('Esfera', choices=[('Municipal', 'Municipal'),
         ('Estadual', 'Estadual'), ('Federal', 'Federal'),
         ('Internacional','Internacional')], validators=[DataRequired()])
     categorie = StringField(_l('Categoria'), validators=[DataRequired()])
-    description = TextAreaField(_l('Descrição:'), validators=[DataRequired(), Length(min=0, max=200)])
-    officialLink = StringField(_l('Link Oficial:'), validators=[DataRequired('URL verificada!'),
+    description = TextAreaField(_l('Descrição'), validators=[DataRequired(),
+        Length(min=0, max=150)], render_kw={"placeholder": "Digite uma descrição para a fonte de dados"})
+    officialLink = StringField(_l('Link Oficial'), validators=[DataRequired('URL verificada!'),
         Regexp('^(http|https):\/\/[\w.\-]+(\.[\w.\-]+)+.*$', 0,
-               'URL inválida')])
-    submit = SubmitField(_l('Enviar'))
+               'URL inválida')], render_kw={"placeholder": "Digite a URL da fonte de dados \
+(https://www.exemplo.com/)"})
+    submit = SubmitField(_l('Registrar'))
 
 
 class SoftwareForm(FlaskForm):
     title = StringField(_l('Título'), validators=[DataRequired()])
-    tag = StringField(_l('Palavra-Chave:'), validators=[DataRequired()])
+    tag = StringField(_l('Palavra-Chave'), validators=[DataRequired()])
     categorie = StringField(_l('Categoria'), validators=[DataRequired()])
-    license = StringField(_l('Licença:'), validators=[DataRequired()])
+    license = StringField(_l('Licença'), validators=[DataRequired()])
     owner = StringField(_l('Proprietário:'), validators=[DataRequired()])
-    activeDevelopment = StringField(_l('Desenvolvedor Ativo:'), validators=[DataRequired(), Length(min=0, max=200)])
-    downloadLink = StringField(_l('Link para Download:'), validators=[DataRequired()])
-    dateCreation = StringField(_l('Data de Criação:'), validators=[DataRequired()])
+    activeDevelopment = StringField(_l('Desenvolvedor Ativo'), validators=[DataRequired(), Length(min=0, max=200)])
+    downloadLink = StringField(_l('Link para Download'), validators=[DataRequired()])
+    dateCreation = StringField(_l('Data de Criação'), validators=[DataRequired()])
     description = TextAreaField(_l('Descrição'), validators=[DataRequired()])
     submit = SubmitField(_l('Enviar'))
 
@@ -66,7 +71,7 @@ class SoftwareForm(FlaskForm):
 class SimilarForm(FlaskForm):
     name =  StringField(_l('Semelhante:'), id='autocomplete', validators=[DataRequired(),
         Length(min=0, max=100)], render_kw={"placeholder": "Digite o nome de um título.."})
-    submit = SubmitField(_l('Salvar'))
+    submit = SubmitField(_l('Registrar'))
 
 
 class BooleanSimilarForm(FlaskForm):
