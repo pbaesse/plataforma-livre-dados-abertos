a18adda3f06d176d1f119c5536bebd462a6b6732
mariacarolinass <m.carolina.soares1@gmail.com>
2020-08-04 02:37:45 -0300

    add tabela similar

--- plataforma.py
+++ plataforma.py
@@ -1,9 +1,10 @@
 from app import create_app, db, cli
-from app.models import User, Post, Software, Similar
+from app.models import User, Post, Software, Similar, Comment, Report
 
 app = create_app()
 cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Software': Software, 'Similar': Similar, 'Comment': Comment}
+    return {'db': db, 'User': User, 'Post': Post, 'Software': Software,
+        'Similar': Similar, 'Comment': Comment, 'Report': Report}
