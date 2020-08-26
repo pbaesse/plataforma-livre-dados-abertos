938de87e40f85c0b5bb3a4208486b047e9d78aa9
mariacarolinass <m.carolina.soares1@gmail.com>
2020-08-26 00:46:09 -0300

    add modelos tag e category

--- plataforma.py
+++ plataforma.py
@@ -1,5 +1,5 @@
 from app import create_app, db, cli
-from app.models import User, Source, Software, Similar, Comment, Report
+from app.models import User, Source, Software, Tag, Category, Comment, Report
 
 app = create_app()
 cli.register(app)
@@ -7,4 +7,5 @@ cli.register(app)
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Source': Source, 'Software': Software,
-        'Similar': Similar, 'Comment': Comment, 'Report': Report}
+        'Tag': Tag, 'Category': Category, 'Comment': Comment,
+        'Report': Report}
